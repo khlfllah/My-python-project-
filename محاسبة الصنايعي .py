d1a395291871ebd98781e5d0d9f475a8c1e0febc
khlfllah <khlfallhthman411@gmail.com>
2025-10-27 12:56:00 +0200

    Update محاسبة الصنايعي .py

--- محاسبة الصنايعي .py
+++ محاسبة الصنايعي .py
@@ -1,8 +1,8 @@
 #مشروع محاسبة الصنايعي
-leng = float(input(" enter the lengt :\n"))
+length = float(input(" enter the lengt :\n"))
 width = float(input(" enter the width : \n"))
-area = leng * width
-mony = float(input("how much the 1 meter : \n"))
+area = length * width
+money = float(input("how much the 1 meter : \n"))
 print (f"the area is : {area} " )
-all_mony = area * mony
-print (f"the mony is :  {all_mony}$")
+all_money = area * money 
+print (f"the mony is :  {all_money}$")
