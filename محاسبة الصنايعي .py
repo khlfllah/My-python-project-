1f6e82ee2a010976a187f3ecaf733116e794ade8
khlfllah <khlfallhthman411@gmail.com>
2025-10-27 13:09:22 +0200

    Update محاسبة الصنايعي .py

--- محاسبة الصنايعي .py
+++ محاسبة الصنايعي .py
@@ -1,6 +1,6 @@
 #مشروع محاسبة الصنايعي
 #ادخال الطول و العرض و قيمة المتر  من المستخدم 
-length = float(input(" enter the lengt :\n"))
+length = float(input(" enter the length :\n"))
 width = float(input(" enter the width : \n"))
 money = float(input("how much the 1 meter : \n"))
 #حساب المساحة و قيمة العمل 
@@ -8,4 +8,4 @@ area = length * width
 all_money = area * money 
 # طباعة المساحة و القيمة النهائية للعمل 
 print (f"the area is : {area} " )
-print (f"the mony is :  {all_money}$")
+print (f"the money is :  {all_money}$")
